fb0e1de786d21589a09e4f30787c82c24ccba977
David Auclair <davaucl@gmail.com>
2018-07-05 17:35:42 -0400

    creating the files

---
+++ src/ui/CoveoMap.ts
@@ -0,0 +1,13 @@
+import {
+    Component,
+    ComponentOptions,
+    IComponentBindings,
+    $$,
+    QueryEvents,
+    IBuildingQueryEventArgs,
+    Initialization
+  } from 'coveo-search-ui';
+
+export class CoveoMap extends Component {
+    static ID = 'CoveoMap';
+}
