38dd620078be58f7af22b3799bc929ecdd07d544
David Auclair <davaucl@gmail.com>
2018-07-06 10:20:28 -0400

    initial MAP

--- src/ui/CoveoMap.ts
+++ src/ui/CoveoMap.ts
@@ -3,11 +3,30 @@ import {
     ComponentOptions,
     IComponentBindings,
     $$,
+    IStringMap,
     QueryEvents,
     IBuildingQueryEventArgs,
     Initialization
   } from 'coveo-search-ui';
 
+interface IMapDefinition {
+    mapElement: HTMLElement;
+}
+
 export class CoveoMap extends Component {
     static ID = 'CoveoMap';
+
+    private tabsDefinition: IStringMap<IMapDefinition> = {};
+    constructor(public element: HTMLElement, public bindings: IComponentBindings) {
+        super(element, CoveoMap.ID, bindings);
+
+        this.initMap();
+    }
+
+    public initMap() {
+        const map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: -34.397, lng: 150.644},
+            zoom: 8
+          });
+    }
 }
