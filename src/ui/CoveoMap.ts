f6c14f3275793a36fcb85e243e1a2c21948d2e26
Unknown <vbernard@coveo.com>
2018-07-23 22:45:23 -0400

    Auto stash before merge of "master" and "origin/master"
    focus on marker, get on top of page, persistent queries

--- src/ui/CoveoMap.ts
+++ src/ui/CoveoMap.ts
@@ -7,7 +7,9 @@ import {
     InitializationEvents,
     IQuerySuccessEventArgs,
     IQueryResult,
-    result,
+    IQueryResults,
+    IQueryBuilderExpression,
+    ExpressionBuilder,
 } from 'coveo-search-ui';
 
 export class CoveoMap extends Component {
@@ -17,30 +19,27 @@ export class CoveoMap extends Component {
     private cluster: MarkerClusterer;
     private markersToCluster = [];
     private infoWindow: google.maps.InfoWindow;
-    private popUpInfo = '';
 
     constructor(public element: HTMLElement, public bindings: IComponentBindings) {
         super(element, CoveoMap.ID, bindings);
         this.markers = {};
-        this.bind.onRootElement(QueryEvents.buildingQuery, (args: IBuildingQueryEventArgs) => this.onBuildingQuery(args));
+        this.bind.onRootElement(QueryEvents.buildingQuery, (args: IQueryBuilderExpression) => this.onBuildingQuery(args));
         this.bind.onRootElement(QueryEvents.querySuccess, (args: IQuerySuccessEventArgs) => this.onQuerySuccess(args));
         this.bind.onRootElement(InitializationEvents.afterInitialization, () => this.initMap());
     }
 
     private onQuerySuccess(args: IQuerySuccessEventArgs) {
         this.clearRelevantMarker();
-        this.plotItem(args);
+        this.plotItem(args.results);
         this.initCluster(args);
     }
 
-    private onBuildingQuery(args: IBuildingQueryEventArgs) {
-        if (this.searchInterface.queryController.firstQuery) {
-            args.queryBuilder.numberOfResults = 1000;
-        }
+    private onBuildingQuery(args: IQueryBuilderExpression) {
+        console.log('Building Query');
     }
 
     private initCluster(args: IQuerySuccessEventArgs) {
-        this.cluster = new MarkerClusterer(this.googleMap, this.markersToCluster, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m', minimumClusterSize: 50 });
+        this.cluster = new MarkerClusterer(this.googleMap, this.markersToCluster, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m', minimumClusterSize: 40 });
     }
 
     private initMap() {
@@ -48,22 +47,21 @@ export class CoveoMap extends Component {
             center: { lat: -33.839, lng: 151.211 },
             zoom: 12
         });
+        Coveo.SearchEndpoint.endpoints.default.search({ q: '', numberOfResults: 1000, pipeline: 'persistent' }).then((results) => this.plotItem(results));
     }
 
-    private plotItem(args: IQuerySuccessEventArgs) {
-        for (const result of args.results.results) {
+    private plotItem(args: IQueryResults) {
+        for (const result of args.results) {
             const marker = this.getMarker(result);
             marker.setOpacity(1);
+            this.focusOnMarker(args.results[0].raw.mapuniqid);
             this.markersToCluster.push(marker);
-        }
-        if (args.results.results[0]) {
-            this.centerMapOnPoint(args.results.results[0]);
         }
     }
 
     private populateInfoWindow(result: IQueryResult) {
         this.infoWindow = new google.maps.InfoWindow({
-            content : '<h2>' + result.raw.businessname + '</h2>' + '<div>' + result.raw.streetname + '<br>' + result.raw.city + '<br>'  + result.raw.state + '<br>' + result.raw.phone + '</div>'
+            content : '<h2>' + result.raw.businessname + '</h2>' + '</h2>' + '<h3>$' + result.raw.price.toLocaleString() + '</h3><br>' + result.raw.city + '<br>'  + result.raw.state + '<br>' + result.raw.phone
         });
     }
 
@@ -84,18 +82,37 @@ export class CoveoMap extends Component {
             this.populateInfoWindow(result);
             this.infoWindow.open(this.googleMap, marker);
         });
+        marker.set('mapuniqid', result.raw.mapuniqid);
         marker.setMap(this.googleMap);
         return marker;
     }
 
     private clearRelevantMarker() {
         Object.keys(this.markers).forEach((key) => {
-            this.markers[key].setOpacity(0.1);
+            this.markers[key].setOpacity(0.2);
         });
     }
 
-    private centerMapOnPoint(resultPosition: IQueryResult) {
-        this.googleMap.setCenter({ lat: resultPosition.raw.latitude, lng: resultPosition.raw.longitude });
+    private setZoomLevel(zoomLevel) {
+        this.googleMap.setZoom(zoomLevel);
+    }
+
+    public focusOnMarker(uniqueId) {
+        if (this.infoWindow) {
+            this.infoWindow.close();
+        }
+        Object.keys(this.markers).forEach((key) => {
+            if (this.markers[key]['mapuniqid'] == uniqueId) {
+                this.setZoomLevel(19);
+                this.centerMapOnPoint(this.markers[key].getPosition()['lat'](), this.markers[key].getPosition()['lng']());
+                google.maps.event.trigger(this.markers[key], 'click');
+            }
+        });
+        document.getElementById('CoveoMap').scrollIntoView();
+    }
+
+    public centerMapOnPoint(latitude, longitude) {
+        this.googleMap.setCenter({ lat: latitude + 0.01, lng: longitude });
     }
 }
 
