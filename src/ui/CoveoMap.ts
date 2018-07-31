4e2e0b4e7d95a22623322c507ce83e2144024bf1
Unknown <vbernard@coveo.com>
2018-07-31 13:44:21 -0400

    Commenting code and fixing HTML search page.

--- src/ui/CoveoMap.ts
+++ src/ui/CoveoMap.ts
@@ -14,12 +14,17 @@ import {
     QueryController,
     AdvancedSearch,
     HtmlTemplate,
+    result,
 } from 'coveo-search-ui';
 
 export interface ICoveoMapOptions {
     template: Template;
 }
 
+/**
+ * Interface used to combine Google Map Markers, their Info Window
+ * and the corresponding Coveo Results
+ */
 interface IResultMarker {
     id: string;
     result: IQueryResult;
@@ -28,19 +33,33 @@ interface IResultMarker {
     isOpen: boolean;
 }
 
+/**
+ * The Coveo Map component, extending the Coveo Framework
+ */
 export class CoveoMap extends Component {
     static ID = 'Map';
+    /**
+     * This section will fetch the data-template-id value of the CoveoMap component
+     * and will load any Underscore template script available in the SearchInterface node
+     * having the matching DOM ID.
+     */
     static options: ICoveoMapOptions = {
         template: ComponentOptions.buildTemplateOption({
             defaultFunction: () => TemplateCache.getDefaultTemplate('Default')
         })
     };
 
+    /**
+     * The CoveoMap object stores the Google Map object, so all the map functionalities are accessible.
+     * All the results are also store in-memory using the Interface defined at the beggining of this file.
+     */
     private googleMap: google.maps.Map;
     private resultMarkers: { [key: string]: IResultMarker };
-    private markersToCluster = [];
-    private searchArea: boolean;
 
+    /**
+     * The CoveoMap object stores the Google Map object, so all the map functionalities are accessible.
+     * All the results are also store in-memory using the Interface defined at the beggining of this file.
+     */
     constructor(public element: HTMLElement, public options: ICoveoMapOptions, public bindings: IComponentBindings) {
         super(element, CoveoMap.ID, bindings);
         this.options = ComponentOptions.initComponentOptions(element, CoveoMap, options);
@@ -74,9 +93,10 @@ export class CoveoMap extends Component {
         // get distance for each result relative to the user point of view
         queryBuilder.advancedExpression.add('$qf(function:\'dist(@latitude, @longitude,' + currentLatitude + ',' + currentLongitude + ')/1000\', fieldName: \'distance\')');
         // adjust item score based on distance
-        queryBuilder.advancedExpression.add('$qrf(expression:\'300 - @distance\')');
+        // queryBuilder.advancedExpression.add('$qrf(expression:\'300 - @distance^0.72\')');
+        queryBuilder.advancedExpression.add('$qrf(expression:\'(300-@distance^0.72)\')');
         // adjust item score based on ranking
-        queryBuilder.advancedExpression.add('$qrf(expression:\'@ratings*10\')');
+        queryBuilder.advancedExpression.add('$qrf(expression:\'@ratings^2.5\')');
     }
     private onQuerySuccess(args: IQuerySuccessEventArgs) {
         this.closeAllInfoWindows();
@@ -95,12 +115,12 @@ export class CoveoMap extends Component {
         for (const result of args.results) {
             const resultMarker = this.getResultMarker(result);
             resultMarker.result = result;
-            resultMarker.marker.setOpacity(1);
             resultMarker.marker.setIcon('http://www.osteokinesis.it/img/icons/map-marker.png');
-            this.markersToCluster.push(resultMarker.marker);
+            resultMarker.marker.setOpacity(1);
             if (args.pipeline != 'persistent' && args.totalCount > 0) {
-                const { lat, lng } = resultMarker.marker.getPosition().toJSON();
-                this.centerMapOnPoint(lat, lng);
+                if (result.index == 0) {
+                    this.focusOnMarker(resultMarker.result.raw.markerid);
+                }
             }
         }
     }
@@ -119,9 +139,7 @@ export class CoveoMap extends Component {
         const resultMarker: IResultMarker = {
             marker, result, isOpen: false, id: result.raw.markerid
         };
-
         this.attachInfoWindowOnClick(resultMarker);
-
         return resultMarker;
     }
 
@@ -154,8 +172,8 @@ export class CoveoMap extends Component {
                     this.closeAllInfoWindows();
                     infoWindow.setContent(element);
                     infoWindow.open(this.googleMap, marker);
-                    this.sendClickEvent(resultMarker);
                     resultMarker.isOpen = true;
+                    this.sendClickEvent(resultMarker);
                 });
             } else {
                 resultMarker.isOpen = false;
@@ -184,13 +202,13 @@ export class CoveoMap extends Component {
     }
 
     private sendClickEvent(resultMarker: IResultMarker) {
-        const customEventCause = {name: 'Click', type: 'document'};
+        const customEventCause = { name: 'Click', type: 'document' };
         let relevant = 'true';
         if (resultMarker.marker.getOpacity() != 1) {
             relevant = 'false';
         }
-        const metadata = {relevantMarker: relevant, department: resultMarker.result.raw.department, businessname: resultMarker.result.raw.businessname, city: resultMarker.result.raw.city, state: resultMarker.result.raw.state, price: resultMarker.result.raw.price};
-        Coveo.logCustomEvent(document.body, customEventCause, metadata);
+        const metadata = { relevantMarker: relevant, department: resultMarker.result.raw.department, businessname: resultMarker.result.raw.businessname, city: resultMarker.result.raw.city, state: resultMarker.result.raw.state, price: resultMarker.result.raw.price };
+        Coveo.logClickEvent(document.body, customEventCause, metadata, resultMarker.result);
     }
 
     private setZoomLevel(zoomLevel) {
@@ -198,21 +216,22 @@ export class CoveoMap extends Component {
     }
 
     public centerMapOnPoint(latitude, longitude) {
-        this.googleMap.setCenter({ lat: latitude + 0.010, lng: longitude });
+        this.googleMap.setCenter({ lat: latitude, lng: longitude });
     }
 
 
     public focusOnMarker(markerId: string) {
         Object.keys(this.resultMarkers)
-        .filter(key => this.resultMarkers[key].id == markerId)
-        .forEach((key) => {
-            const { marker } = this.resultMarkers[key];
-            // this.setZoomLevel(14);
-            const { lat, lng } = marker.getPosition().toJSON();
-            this.centerMapOnPoint(lat, lng);
-            google.maps.event.trigger(marker, 'click');
-            marker.setAnimation(google.maps.Animation.DROP);
-        });
+            .filter(key => this.resultMarkers[key].id == markerId)
+            .forEach((key) => {
+                const { marker } = this.resultMarkers[key];
+                // this.setZoomLevel(14);
+                const { lat, lng } = marker.getPosition().toJSON();
+                this.centerMapOnPoint(lat, lng);
+                google.maps.event.trigger(marker, 'click');
+                marker.setAnimation(google.maps.Animation.DROP);
+                this.sendClickEvent(this.resultMarkers[key]);
+            });
         this.element.scrollIntoView();
     }
 }
