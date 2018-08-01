957bf7ff114396fce7201934f56b958e8323bcce
Unknown <vbernard@coveo.com>
2018-08-01 16:11:35 -0400

    Code Refactoring with FLG

--- src/ui/CoveoMap.ts
+++ src/ui/CoveoMap.ts
@@ -7,7 +7,6 @@ import {
     InitializationEvents,
     IQuerySuccessEventArgs,
     IQueryResult,
-    IQueryResults,
     ComponentOptions,
     Template,
     TemplateCache
@@ -18,7 +17,7 @@ export interface ICoveoMapOptions {
 }
 
 /**
- * Interface used to combine Google Map Markers, their Info Window
+ * Interface used to combine Google Map Markers, Google Info Window
  * and the corresponding Coveo Results
  */
 interface IResultMarker {
@@ -30,7 +29,7 @@ interface IResultMarker {
 }
 
 /**
- * The Coveo Map component, extending the Coveo Framework
+ * The Coveo Map component Class, extending the Coveo Framework Component
  */
 export class CoveoMap extends Component {
     static ID = 'Map';
@@ -53,9 +52,6 @@ export class CoveoMap extends Component {
     private googleMap: google.maps.Map;
     private resultMarkers: { [key: string]: IResultMarker };
 
-    /**
-     * The constructor binds the CoveoMap behaviour to the Coveo Query
-     */
     constructor(public element: HTMLElement, public options: ICoveoMapOptions, public bindings: IComponentBindings) {
         super(element, CoveoMap.ID, bindings);
         this.options = ComponentOptions.initComponentOptions(element, CoveoMap, options);
@@ -65,9 +61,6 @@ export class CoveoMap extends Component {
         this.bind.onRootElement(InitializationEvents.afterInitialization, () => this.initMap());
     }
 
-    /**
-     * The map initialization is done after Coveo Components initialization.
-     */
     private initMap() {
         this.googleMap = new google.maps.Map(this.element, {
             center: { lat: -33.839, lng: 151.211 },
@@ -76,85 +69,65 @@ export class CoveoMap extends Component {
         this.getPersistentMarkers();
     }
 
-    /**
-     * Relevance options are injected in the query.
-     */
     private onBuildingQuery(args: IBuildingQueryEventArgs) {
         const queryBuilder = args.queryBuilder;
         const currentLatitude = this.googleMap.getCenter()['lat']();
         const currentLongitude = this.googleMap.getCenter()['lng']();
         // get distance for each result relative to the user point of view
-        queryBuilder.advancedExpression.add('$qf(function:\'dist(@latitude, @longitude,' + currentLatitude + ',' + currentLongitude + ')/1000\', fieldName: \'distance\')');
+        queryBuilder.advancedExpression.add(`$qf(function:'dist(@latitude, @longitude, ${currentLatitude}, ${currentLongitude})/1000', fieldName: 'distance')`);
         // adjust item score based on their distance
-        queryBuilder.advancedExpression.add('$qrf(expression:\'(400-@distance^0.72)\')');
+        queryBuilder.advancedExpression.add(`$qrf(expression:'(500-@distance^0.72)')`);
         // adjust item score based on their rankings
-        queryBuilder.advancedExpression.add('$qrf(expression:\'@ratings*5\')');
+        queryBuilder.advancedExpression.add(`$qrf(expression:'@ratings*5')`);
     }
 
-    /**
-     * Modify the markers when a query comes back from Coveo
-     */
     private onQuerySuccess(args: IQuerySuccessEventArgs) {
         this.closeAllInfoWindows();
         this.clearRelevantMarker();
-        this.plotItems(args.results);
-    }
-
-    /**
-     * This function request 1000 results from Coveo Cloud using the persistent pipeline.
-     */
-    private getPersistentMarkers() {
-        Coveo.SearchEndpoint.endpoints.default.search({ q: '', numberOfResults: 1000, pipeline: 'persistent' }).then((results) => {
-            results.results.forEach(result => {
-                result.searchInterface = this.searchInterface;
-                result.index = 101;
-            });
-            this.plotItems(results);
+        args.results.results.forEach(result => {
+            const resultMarker = this.plotItem(result);
+            this.plotItemAsRelevant(resultMarker);
         });
     }
 
-    /**
-     *  For each results, change the marker status (relevant vs background marker)
-     *  this will also create the marker or fetch an already created marker
-     */
-    private plotItems(args: IQueryResults) {
-        for (const result of args.results) {
-            const resultMarker = this.getResultMarker(result);
-            resultMarker.result = result;
-            resultMarker.marker = this.setMarkersAsBackground(resultMarker.marker);
-            if (args.pipeline != 'persistent' && args.totalCount > 0) {
-                resultMarker.marker = this.setMarkersAsRelevant(resultMarker.marker);
-                if (result.index == 0) {
-                    this.centerMapOnPoint(resultMarker.result.raw.latitude, resultMarker.result.raw.longitude);
-                }
-            }
+    private getPersistentMarkers() {
+        // Get 1000 results to load background items on the map. The call is using the Search Endpoint directly.
+        Coveo.SearchEndpoint.endpoints.default.search({ q: '', numberOfResults: 1000, pipeline: 'persistent' }).then((results) => {
+            // Get 1000 results to load background items on the map.
+            results.results.forEach(result => {
+                result.searchInterface = this.searchInterface;
+                result.index = 101;
+                this.plotItem(result);
+            });
+        });
+    }
+
+    private plotItem(result: IQueryResult): IResultMarker {
+        const resultMarker = this.getResultMarker(result);
+        resultMarker.result = result;
+        this.setMarkerAsBackground(resultMarker.marker);
+        return resultMarker;
+    }
+
+    private plotItemAsRelevant(resultMarker: IResultMarker) {
+        this.setMarkerAsRelevant(resultMarker.marker);
+        if (resultMarker.result.index == 0) {
+            this.centerMapOnPoint(resultMarker.result.raw.latitude, resultMarker.result.raw.longitude);
         }
     }
 
-    /**
-     *  Modify the marker to make it look blue with full opacity.
-     */
-    private setMarkersAsRelevant(marker: google.maps.Marker) {
+    private setMarkerAsRelevant(marker: google.maps.Marker) {
         marker.setIcon('http://www.osteokinesis.it/img/icons/map-marker.png');
         marker.setOpacity(1);
         marker.setZIndex(100);
-        return marker;
     }
 
-    /**
-     *  Modify the marker to make it red with low opacity.
-     */
-    private setMarkersAsBackground(marker: google.maps.Marker) {
+    private setMarkerAsBackground(marker: google.maps.Marker) {
         marker.setIcon(null);
         marker.setOpacity(0.2);
         marker.setZIndex(50);
-        return marker;
     }
 
-    /**
-     *  This holds all the markers, and creates a new one of the requested marker
-     *  doesn't exist.
-     */
     private getResultMarker(result: IQueryResult): IResultMarker {
         const key = result.raw.sysrowid;
         if (!this.resultMarkers[key]) {
@@ -163,9 +136,6 @@ export class CoveoMap extends Component {
         return this.resultMarkers[key];
     }
 
-    /**
-     *  Create a result marker, and the related InfoWindow
-     */
     private createResultMarker(result: IQueryResult): IResultMarker {
         const marker = this.createMarker(result);
 
@@ -176,9 +146,6 @@ export class CoveoMap extends Component {
         return resultMarker;
     }
 
-    /**
-     *  Create a Google Map marker
-     */
     private createMarker(result: IQueryResult): google.maps.Marker {
         const marker = new google.maps.Marker({
             position: {
@@ -192,9 +159,6 @@ export class CoveoMap extends Component {
         return marker;
     }
 
-    /**
-     *  Manage the InfoWindow attached with a Marker in the ResultMarker item.
-     */
     private attachInfoWindowOnClick(resultMarker: IResultMarker) {
         const { marker } = resultMarker;
         marker.addListener('click', () => {
@@ -221,21 +185,14 @@ export class CoveoMap extends Component {
         });
     }
 
-    /**
-     *  Instantiate the Coveo Result Template in the InfoWindow.
-     */
     private instantiateTemplate(result: IQueryResult): Promise<HTMLElement> {
         return this.options.template.instantiateToElement(result).then(element => {
             Component.bindResultToElement(element, result);
-            return Initialization.automaticallyCreateComponentsInsideResult(element, result).initResult.then(() => {
-                return element;
-            });
+            return Initialization.automaticallyCreateComponentsInsideResult(element, result)
+                .initResult.then(() => element);
         });
     }
 
-    /**
-     *  Closes the opened InfoWindows
-     */
     private closeAllInfoWindows() {
         Object.keys(this.resultMarkers)
             .map(key => this.resultMarkers[key])
@@ -246,34 +203,28 @@ export class CoveoMap extends Component {
             });
     }
 
-    /**
-     *  Toggle a relevant marker to a background marker
-     */
     private clearRelevantMarker() {
         Object.keys(this.resultMarkers).forEach((key) => {
             const { marker } = this.resultMarkers[key];
-            marker.setOpacity(0.2);
-            marker.setZIndex(90);
-            marker.setIcon(null);
+            this.setMarkerAsBackground(marker);
         });
     }
 
-    /**
-     *  Send a click event to Coveo using custom MetaData
-     */
     private sendClickEvent(resultMarker: IResultMarker) {
         const customEventCause = { name: 'Click', type: 'document' };
-        let relevant = 'true';
-        if (resultMarker.marker.getOpacity() != 1) {
-            relevant = 'false';
-        }
-        const metadata = { relevantMarker: relevant, department: resultMarker.result.raw.department, businessname: resultMarker.result.raw.businessname, city: resultMarker.result.raw.city, state: resultMarker.result.raw.state, price: resultMarker.result.raw.price };
-        this.usageAnalytics.logClickEvent(customEventCause, metadata, resultMarker.result, this.element);
+        const { marker, result } = resultMarker;
+        const isRelevant = marker.getOpacity() != 1;
+        const metadata = {
+            relevantMarker: isRelevant,
+            department: result.raw.department,
+            businessname: result.raw.businessname,
+            city: result.raw.city,
+            state: result.raw.state,
+            price: result.raw.price
+        };
+        this.usageAnalytics.logClickEvent(customEventCause, metadata, result, this.element);
     }
 
-    /**
-     *  Centers map on point and offset it to let InfoWindow visible
-     */
     private centerMapOnPoint(lat, lng) {
         const scale = Math.pow(2, this.googleMap.getZoom());
         const latLng = new google.maps.LatLng(lat, lng);
@@ -287,9 +238,6 @@ export class CoveoMap extends Component {
         this.googleMap.setCenter(newCenter);
     }
 
-    /**
-     *  Open a Infowindow and adjust the Map
-     */
     public focusOnMarker(markerId: string) {
         Object.keys(this.resultMarkers)
             .filter(key => this.resultMarkers[key].id == markerId)
